33a7e2e3f83ef2a22f87467343ba3c4f2bc0dbe4
Владимир Башлыков <bashlikovvaldimir@gmail.com>
2024-10-01 16:48:41 +0300

    fix: code cleaned;

--- features/home/src/main/kotlin/by/bashlikovvv/home/presentation/ui/store/HomeStoreExecutor.kt
+++ features/home/src/main/kotlin/by/bashlikovvv/home/presentation/ui/store/HomeStoreExecutor.kt
@@ -3,23 +3,25 @@ package by.bashlikovvv.home.presentation.ui.store
 import by.bashlikovvv.home.presentation.ui.store.HomeStore.*
 import by.bashlikovvv.home.presentation.ui.store.HomeStoreFactory.*
 import by.bashlikovvv.ui.base.BaseCoroutineExecutor
-import by.bashlikovvv.home.data.remote.RootRepository
+import by.bashlikovvv.home.data.remote.HomeRepository
 import by.bashlikovvv.domain.model.WearableEvent
 import by.bashlikovvv.domain.model.VibrationDescriptor
 import by.bashlikovvv.domain.model.VibrationAction
 import org.koin.core.component.inject
 
-class HomeStoreExecutor : BaseCoroutineExecutor<Intent, Action, State, Msg, Label>() {
-    private val rootRepository: RootRepository by inject()
+class HomeStoreExecutor : BaseCoroutineExecutor<Intent, Nothing, State, Msg, Label>() {
+    private val homeRepository: HomeRepository by inject()
 
     override fun executeIntent(intent: Intent, getState: () -> State) {
         when (intent) {
             is Intent.Vibrate -> simpleVibrate(intent)
+            is Intent.Initialize -> homeRepository.initialize(intent.context)
+            is Intent.Destroy -> homeRepository.destroy()
         }
     }
 
     private fun simpleVibrate(intent: Intent.Vibrate) {
-        rootRepository.dispatchEvent(testVibration)
+        homeRepository.dispatchEvent(testVibration)
     }
 
     private val testVibration = WearableEvent(
